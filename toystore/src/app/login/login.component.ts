df15fc91ecd0f49e45a28b97eaf5b1112627a05d
Gautam Chawhan <gautamgc95@gmail.com>
2024-12-28 14:45:38 +0530

    login page half implementation

--- toystore/src/app/login/login.component.ts
+++ toystore/src/app/login/login.component.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule,MatCardModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+
+
+  hide = signal(true);
+  profileForm!: FormGroup<any>;
+
+  constructor(private fb: FormBuilder) {
+    
+  }
+
+
+
+
+  ngOnInit(): void {
+   this.profileForm=this.fb.group({
+    username:[],
+    password:[]
+
+   })
+  }
+
+
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+
+  onSubmit() {
+    throw new Error('Method not implemented.');
+  }
 
 }
---
+++ toystore/src/app/model/login.model.ts
@@ -0,0 +1,15 @@
+export class Login {
+
+    userId?: number;
+    userName?: string;
+    emailId?: string;
+    phoneNumber?: number;
+    password?: string;
+    reEnterPassword?: string;
+    roles?: string;
+    updatedBy?: string;
+    updatedDate?: Date;
+    createdBy?: string;
+    createdDate?: Date;
+
+}
