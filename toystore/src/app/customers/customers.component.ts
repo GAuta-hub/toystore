50a473e6825839a1b5c68e341161867f829688b1
nachiketchauhan <nachiketchauhan1019@gmail.com>
2025-01-01 14:55:23 +0530

    actions implements

--- toystore/src/app/customers/customers.component.ts
+++ toystore/src/app/customers/customers.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { AgGridModule } from 'ag-grid-angular';
+import { ClientSideRowModelModule, ColDef, Module } from 'ag-grid-community';
+import { CustomerService } from '../service/customer.service';
+import { ActionComponent } from '../action/action.component';
 
 @Component({
   selector: 'app-customers',
@@ -8,6 +11,40 @@ import { AgGridModule } from 'ag-grid-angular';
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.scss'
 })
-export class CustomersComponent {
+export class CustomersComponent implements OnInit {
+  rowData:any;
+  constructor(private customerservices:CustomerService){}
+ ngOnInit(): void {
+   this.customerservices.getCustomerList().subscribe({
+    next:(data:any)=>{
+      this.rowData=data
+    },
+    error:(error:any)=>{}
+   })
+ }
+ modules: Module[] = [ClientSideRowModelModule];
 
+
+
+ coldefs:ColDef[] = [
+     {
+     headerName:"Select",
+       editable: true,
+       cellRenderer: (params: { value: any; }) => {
+         return `<input type='checkbox' ${params.value ? 'checked' : ''} />`;
+       }
+     },
+   {field: "Action",
+     cellRenderer:ActionComponent,
+   },
+     { field: "customerId" },
+     { field:"firstName"},
+     {field:"lastName"},
+     {field:"email"},
+     {field:"phoneNumber"},
+     {field:"address"},
+     {field:"dateRegistered"},
+     {field:"fieldNam"}
+   ]
+ 
 }
--- toystore/src/app/service/customer.service.ts
+++ toystore/src/app/service/customer.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Customer } from '../model/customer.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  a:string="http://192.168.1.104:8080";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  getCustomerList() :Observable<Customer>{
+    return this.http.get(this.a+"/getCustomerslist");
+  }
 }
