88c21767696f106ace8f8d5457a6af6f720dae14
nachiketchauhan <nachiketchauhan1019@gmail.com>
2025-01-01 14:38:25 +0530

    added button in customer

--- toystore/src/app/customers/customers.component.ts
+++ toystore/src/app/customers/customers.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { AgGridModule } from 'ag-grid-angular';
 
 @Component({
   selector: 'app-customers',
-  imports: [],
+  imports: [MatButtonModule,AgGridModule],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.scss'
 })
