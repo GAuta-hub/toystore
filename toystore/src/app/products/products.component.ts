6bc63e39f68f73702093ae9aaaf4639de84a7de0
Gautam Chawhan <gautamgc95@gmail.com>
2025-01-02 03:00:06 +0530

    curd operation halfly done

--- toystore/src/app/products/products.component.ts
+++ toystore/src/app/products/products.component.ts
@@ -1,51 +1,94 @@
 import { Component, OnInit } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular';
-import { ClientSideRowModelModule, ColDef, Module } from 'ag-grid-community';
+import { ClientSideRowModelApiModule, ClientSideRowModelModule, ColDef, GridApi, GridReadyEvent, Module, ModuleRegistry, NumberEditorModule, RowApiModule, RowSelectionModule, SelectionChangedEvent, TextEditorModule, ValidationModule,  } from 'ag-grid-community';
 import { ProductService } from '../service/product.service';
+import { MatButtonModule } from '@angular/material/button';
 import { ActionComponent } from '../action/action.component';
+ModuleRegistry.registerModules([
+  ClientSideRowModelApiModule,
+  RowSelectionModule,
+  RowApiModule,
+  ClientSideRowModelModule,
+  ValidationModule,
+  NumberEditorModule,TextEditorModule
+]);
+
 @Component({
   selector: 'app-products',
-  imports: [AgGridAngular],
+  imports: [AgGridAngular, MatButtonModule,],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent implements OnInit{
-  
+export class ProductsComponent implements OnInit {
+
+
+
+
   modules: Module[] = [ClientSideRowModelModule];
-  rowData:any;
-
-  constructor(private product:ProductService){}
+  rowData:any[]=[] ;
+  gridApi!: GridApi;
  
+
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+  }
+
+  
+  constructor(private product: ProductService) { }
+
   ngOnInit(): void {
-this.product.getproductDetails().subscribe({
-  next: (data:any)=>{
-    this.rowData =data;
-  },
-  error:(data:any)=>{
+    this.product.getproductDetails().subscribe({
+      next: (data: any) => {
+        this.rowData = data;
+      },
+      error: (data: any) => {
 
-  }
-})
+      }
+    })
   }
 
-  coldefs:ColDef[] = [
+  coldefs: ColDef[] = [
     {
-    headerName:"Select",
+      headerName: "Select",
       editable: true,
       cellRenderer: (params: { value: any; }) => {
         return `<input type='checkbox' ${params.value ? 'checked' : ''} />`;
       }
     },
-  {field: "Action",
-    cellRenderer:ActionComponent,
-  },
-    { field: "productId" },
-    { field:"productName"},
-    {field:"description"},
-    {field:"category"},
-    {field:"price"},
-    {field:"stock_quantity"},
-    {field:"manufacturer"},
-    {field:"age_range"}
+    {
+      field: "Action",
+      cellRenderer: ActionComponent,
+    },
+    { field: "productId",editable:false },
+    { field: "productName",editable:true },
+    { field: "description",editable:true },
+    { field: "category",editable:true },
+    { field: "price",editable:true },
+    { field: "stock_quantity",editable:true },
+    { field: "manufacturer",editable:true },
+    { field: "age_range",editable:true }
   ]
 
+ 
+  additems() {
+        
+      this.gridApi.applyTransaction({
+        addIndex:0,
+        add:[0],
+        
+      });
+    }
+    
+    deleteitem() {
+      const selectedData = this.gridApi.getSelectedRows();
+      console.log(selectedData+"asiuyioqaudrwqwrlfhdsoefuw");
+      this.gridApi.applyTransaction({
+
+       remove:[selectedData]
+      })
+      }
+
+      onSelectionData($event: SelectionChangedEvent) {
+        console.log(event+"+++9+++587+++++++++++++++++++++++++++++++++++++++");
+        }
 }
